67c1f49736ae4f7ad11785e700e3913406229ba8
MohamadAlaskari <m_alskari96@hotmail.com>
2024-04-06 15:35:42 +0200

    create core module und authentication module with auth service, guard and interceptor

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AuthenticationModule } from './authentication/authentication.module';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    AuthenticationModule
+  ]
+})
+export class CoreModule { }
