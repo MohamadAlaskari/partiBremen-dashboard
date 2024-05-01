4832617e1b6de4ff1d0f7cf5e4b6b8c3c597542b
MohamadAlaskari <m_alskari96@hotmail.com>
2024-05-01 09:08:15 +0200

    create validation login inputs

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -3,14 +3,20 @@ import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './components/login/login.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ToastService } from '../../shared/services/toast.service';
 import { HttpClientModule } from '@angular/common/http';
 import { AuthService } from './services/auth.service';
 
 @NgModule({
   declarations: [LoginComponent],
-  imports: [CommonModule, AuthRoutingModule, FormsModule, HttpClientModule],
+  imports: [
+    CommonModule,
+    AuthRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+  ],
   exports: [LoginComponent],
   providers: [AuthService, ToastService],
 })
--- src/app/modules/auth/components/login/login.component.ts
+++ src/app/modules/auth/components/login/login.component.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 import { User } from '../../../../shared/models/user.model';
 import { ToastService } from '../../../../shared/services/toast.service';
 import { AuthService } from '../../services/auth.service';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -11,12 +12,21 @@ import { AuthService } from '../../services/auth.service';
 })
 export class LoginComponent {
   user: User = new User('', '', '', '', '', null, '', '', false, '', false);
+  loginForm: FormGroup;
 
   constructor(
     private router: Router,
     private authService: AuthService,
     private toastService: ToastService
-  ) {}
+  ) {
+    this.loginForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(6),
+      ]),
+    });
+  }
 
   ngOnInit() {
     // Automatische Umleitung zur Startseite, wenn bereits angemeldet
@@ -25,23 +35,32 @@ export class LoginComponent {
     }
   }
   onLogin(): void {
-    this.authService.login(this.user.email, this.user.password).subscribe({
-      next: (user) => {
-        this.router.navigate(['/dashboard']);
-        this.toastService.show(
-          'success',
-          'Login Successful',
-          `Welcome back ${user.name}!`
-        );
-      },
-      error: (error) => {
-        this.toastService.show(
-          'error',
-          'Login Failed',
-          'Invalid email or password.'
-        );
-        console.error('Login fehlgeschlagen', error);
-      },
-    });
+    if (this.loginForm.valid) {
+      const { email, password } = this.loginForm.value;
+      this.authService.login(email, password).subscribe({
+        next: (user) => {
+          this.router.navigate(['/dashboard']);
+          this.toastService.show(
+            'success',
+            'Login Successful',
+            `Welcome back ${user.name}!`
+          );
+        },
+        error: (error) => {
+          this.toastService.show(
+            'error',
+            'Login Failed',
+            'Invalid email or password.'
+          );
+          console.error('Login failed', error);
+        },
+      });
+    } else {
+      this.toastService.show(
+        'error',
+        'Validation Error',
+        'Please check your input.'
+      );
+    }
   }
 }
