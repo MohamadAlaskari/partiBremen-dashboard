729b4cd3494e8104d3aedfcc807e57ef464537b4
MohamadAlaskari <m_alskari96@hotmail.com>
2024-05-04 15:11:27 +0200

    fix search und tab user

--- src/app/modules/user-management/components/user-list/user-list.component.ts
+++ src/app/modules/user-management/components/user-list/user-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { UserManagementService } from '../../services/user-management-service/user-management.service';
 import { User } from '../../../../shared/models/user.model';
@@ -9,22 +9,23 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
-  styleUrl: './user-list.component.scss',
+  styleUrls: ['./user-list.component.scss'],
 })
-export class UserListComponent {
+export class UserListComponent implements OnDestroy {
   users: User[] = [];
-  // Konfiguration der Tabs, die in der Ansicht verwendet werden.
+  originalUsers: User[] = [];
   tabConfig = [
     { label: 'View All', value: 'all' },
     { label: 'Admins', value: 'admins' },
     { label: 'Active', value: 'active' },
   ];
-  // Hinzufügen einer Zustandsvariablen für den aktiven Filtertyp
+  //muss wissen nach search value oder tab value muss die daten gefiltert werden
   activeFilterType: 'tab' | 'search' = 'tab';
+
+  currentTabFilter = 'all';
 
   counters: CounterState[] = [];
 
-  // Spaltenüberschriften für die Anzeige in der Table.
   columns: { header: string; key: string }[] = [
     { header: 'Name', key: 'name' },
     { header: 'Surname', key: 'surname' },
@@ -33,14 +34,9 @@ export class UserListComponent {
     { header: 'Role', key: 'role' },
     { header: 'Active', key: 'active' },
   ];
-
-  // Text für das Suchfeld.
   searchText: string = '';
 
-  // Subscription für API-Anfragen.
   private subscriptions: Subscription = new Subscription();
-
-  isSortDropdownActive = false;
 
   constructor(
     private userManagementService: UserManagementService,
@@ -50,21 +46,15 @@ export class UserListComponent {
 
   ngOnInit() {
     this.loadUsers();
-    //  this.setCustomFilterPredicate();
+    this.updateCounters();
   }
 
-  // Umschalten des Zustands des Sortierungs-Dropdown-Menüs.
-  toggleDropdown(): void {
-    this.isSortDropdownActive = !this.isSortDropdownActive;
-  }
-
-  // Laden der Benutzer vom Server.
   loadUsers(): void {
     this.subscriptions.add(
       this.userManagementService.getUsers().subscribe({
         next: (users) => {
-          this.users = users;
-          this.updateCounters();
+          this.originalUsers = users;
+          this.applyFilters(); // Initial filter application after loading users
           this.toastService.show(
             'success',
             'Success',
@@ -73,85 +63,61 @@ export class UserListComponent {
         },
         error: (err) => {
           console.error('Failed to load users', err);
-
           this.toastService.show('error', 'Error', 'Failed to load users');
         },
       })
     );
   }
 
-  // Aktualisieren der Zustandszähler basierend auf den gefilterten Daten.
   updateCounters(): void {
     const activeUsers = this.users.filter((user) => user.active);
     const adminUsers = this.users.filter((user) => user.role === 'ADMIN');
     const verifiedUsers = this.users.filter((user) => user.verified);
-
     this.counters = [
-      { count: activeUsers.length, label: 'Aktiv' },
-      { count: adminUsers.length, label: 'Admin' },
+      { count: activeUsers.length, label: 'Active' },
+      { count: adminUsers.length, label: 'Admins' },
       { count: verifiedUsers.length, label: 'Verified' },
       { count: this.users.length, label: 'Total Users' },
     ];
   }
 
-  // Anpassen des Filterkriteriums der Datenquelle
-  setCustomFilterPredicate(): void {
-    /*
-    this.dataSource.filterPredicate = (data: User, filter: string): boolean => {
-      if (this.activeFilterType === 'search') {
-        // Suchlogik nur anwenden, wenn der aktive Filtertyp 'search' ist
-        return (
-          data.name?.toLowerCase().includes(filter) ||
-          data.surname?.toLowerCase().includes(filter) ||
-          data.email?.toLowerCase().includes(filter)
-        );
-      } else {
-        // Tab-Filterlogik nur anwenden, wenn der aktive Filtertyp 'tab' ist
-        switch (filter) {
-          case 'admins':
-            return data.role === 'ADMIN';
-          case 'active':
-            return data.active === true;
-          case 'all':
-          default:
-            return true; // Kein Filter angewendet, alle Daten anzeigen
-        }
-      }
-    };*/
-  }
-
-  textFilter(data: User, filter: string): boolean {
-    const transformedFilter = filter.trim().toLowerCase();
-    return (
-      data.name?.toLowerCase().includes(transformedFilter) ||
-      data.surname?.toLowerCase().includes(transformedFilter) ||
-      data.email?.toLowerCase().includes(transformedFilter)
-    );
+  applyFilters(): void {
+    let filteredUsers = this.originalUsers.slice(); // Begin with a copy of the full user list
+    if (this.currentTabFilter !== 'all') {
+      filteredUsers = filteredUsers.filter((user) => {
+        if (this.currentTabFilter === 'admins') return user.role === 'ADMIN';
+        if (this.currentTabFilter === 'active') return user.active;
+        return true; // No filter applied, show all data
+      });
+    }
+    if (this.searchText) {
+      const searchTextLower = this.searchText.toLowerCase();
+      filteredUsers = filteredUsers.filter(
+        (user) =>
+          user.name?.toLowerCase().includes(searchTextLower) ||
+          user.surname?.toLowerCase().includes(searchTextLower) ||
+          user.email?.toLowerCase().includes(searchTextLower) ||
+          user.role?.toLocaleLowerCase().includes(searchTextLower)
+      );
+    }
+    this.users = filteredUsers;
   }
 
   filterUsers(): void {
-    /*
-    this.activeFilterType = 'search'; // Setzt den aktiven Filtertyp auf 'search'
-    this.dataSource.filter = this.searchText.trim().toLowerCase();
-    */
+    this.activeFilterType = 'search';
+    this.applyFilters();
   }
 
   handleTabChange(tabValue: string): void {
-    /*
-    this.activeFilterType = 'tab'; // Setzt den aktiven Filtertyp auf 'tab'
-    this.dataSource.filter = tabValue;
-    if (this.dataSource.paginator) {
-      this.dataSource.paginator.firstPage();
-
-    }
-    */
+    this.currentTabFilter = tabValue;
+    this.applyFilters();
   }
-  addUser() {
+
+  addUser(): void {
     this.router.navigate(['/user-management/add-user']);
   }
-  // Bereinigen der Subscriptions beim Zerstören der Komponente.
+
   ngOnDestroy(): void {
     this.subscriptions.unsubscribe();
-    console.log('Cleaned up subscriptions');
   }
 }
