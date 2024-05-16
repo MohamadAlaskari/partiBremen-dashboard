555a2f6f399647b9cb490a4f46848306b35a7a76
MohamadAlaskari <m_alskari96@hotmail.com>
2024-05-16 23:23:21 +0200

    create update delete view

--- src/app/modules/user-management/components/delete-user/delete-user.component.ts
+++ src/app/modules/user-management/components/delete-user/delete-user.component.ts
@@ -1,18 +1,17 @@
-import { Component, Input } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { UserManagementService } from '../../services/user-management-service/user-management.service';
-import { ToastService } from '../../../../shared/services/toast.service';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Component } from '@angular/core';
 import { User } from '../../../../shared/models/user.model';
-import { timeout } from 'rxjs';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ToastService } from '../../../../shared/services/toast.service';
+import { UserManagementService } from '../../services/user-management-service/user-management.service';
 
 @Component({
-  selector: 'app-update-user',
-  templateUrl: './update-user.component.html',
-  styleUrl: './update-user.component.scss',
+  selector: 'app-delete-user',
+  templateUrl: './delete-user.component.html',
+  styleUrl: './delete-user.component.scss',
 })
-export class UpdateUserComponent {
-  title: string = 'Update User';
+export class DeleteUserComponent {
+  title: string = 'Delete User';
   user!: User;
   id: string | null = null;
   userForm!: FormGroup;
@@ -20,12 +19,12 @@ export class UpdateUserComponent {
 
   // Mapping von Rollen zu ihren entsprechenden Zahlenwerten
   roleMapping: { [key: string]: number } = {
-    "DEFAULT": 0,
-    "USER": 1,
-    "CREATOR": 2,
-    "MODERATOR": 3,
-    "ADMIN": 4,
-    "DECIDER": 5
+    DEFAULT: 0,
+    USER: 1,
+    CREATOR: 2,
+    MODERATOR: 3,
+    ADMIN: 4,
+    DECIDER: 5,
   };
 
   constructor(
@@ -65,7 +64,6 @@ export class UpdateUserComponent {
 
   // Methode zur Konvertierung der Rollenbezeichnung in die entsprechende Zahl
   private getRoleValue(role: string): number {
-  
     return this.roleMapping[role] || 0; // Fallback auf 0 (DEFAULT) falls die Rolle nicht gefunden wird
   }
   private loadUser(id: string): void {
@@ -74,34 +72,28 @@ export class UpdateUserComponent {
       this.initForm();
     });
   }
-  onSubmit(): void {
-    this.userFormSubmitted = true;
-    if (this.userForm.valid) {
-      const updatedUser = { ...this.user, ...this.userForm.value };
-      // Log the data being sent
-      console.log('Submitting updated user:', updatedUser);
-      this.userService.updateUser(this.user.id, updatedUser).subscribe({
-        next: () => {
-          console.log('user updated');
-          this.toastService.show(
-            'success',
-            'Success',
-            'User updated successfully'
-          );
-          setTimeout(() => {
-            this.router.navigate(['/user-management']);
-          }, 1000);
-        },
-        error: (error) => {
-          this.toastService.show(
-            'error',
-            'Error',
-            'Failed to update user. Please try again later'
-          );
 
-          console.error('Error updating user:', error);
-        },
-      });
+  deleteUser(userId: string): void {
+    this.userService.deleteUser(userId).subscribe({
+      next: () => {
+        this.toastService.show(
+          'success',
+          'Success',
+          'User deleted successfully'
+        );
+        setTimeout(() => {
+          this.router.navigate(['/user-management']);
+        }, 1000);
+      },
+      error: (err) => {
+        console.error('Failed to delete user', err);
+        this.toastService.show('error', 'Error', 'Failed to delete user');
+      },
+    });
+  }
+  onDelete(): void {
+    if (this.id) {
+      this.deleteUser(this.id);
     }
   }
 
