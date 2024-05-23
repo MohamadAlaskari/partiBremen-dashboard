327c806136ab4a89c34b03874d38420be2f20074
MohamadAlaskari <m_alskari96@hotmail.com>
2024-05-23 17:42:29 +0200

    create mapbox service und inject it in view-user

--- src/app/modules/user-management/components/view-user/view-user.component.ts
+++ src/app/modules/user-management/components/view-user/view-user.component.ts
@@ -12,6 +12,7 @@ import {
 } from '../../../../core/models/partiBremen.model';
 import * as mapboxgl from 'mapbox-gl'; // Korrekt importieren
 import { environment } from '../../../../../environment';
+import { MapboxService } from '../../services/mapbox-service/mapbox.service';
 @Component({
   selector: 'app-view-user',
   templateUrl: './view-user.component.html',
@@ -24,13 +25,11 @@ export class ViewUserComponent {
   userPois: Poi[] = [];
   selectedPoi: Poi | null = null;
 
-  map!: mapboxgl.Map;
-  markers: { [key: string]: mapboxgl.Marker } = {};
-
   constructor(
     private route: ActivatedRoute,
     private userService: UserManagementService,
-    private toastService: ToastService
+    private toastService: ToastService,
+    private mapboxService: MapboxService // Hinzufügen des Mapbox-Services
   ) {}
 
   ngOnInit(): void {
@@ -38,9 +37,7 @@ export class ViewUserComponent {
   }
 
   ngOnDestroy(): void {
-    if (this.map) {
-      this.map.remove();
-    }
+    this.mapboxService.map?.remove();
   }
 
   private extractIdFromRoute(): void {
@@ -77,125 +74,15 @@ export class ViewUserComponent {
     });
   }
 
-  // mapbox
   private initializeMap(): void {
-    this.map = new mapboxgl.Map({
-      accessToken: environment.mapbox.accessToken,
-      container: 'map',
-      style: 'mapbox://styles/mapbox/streets-v12',
-      center: [8.8016936, 53.0792962],
-      zoom: 9,
+    this.mapboxService.initializeMap('map', [8.8016936, 53.0792962], 9);
+    this.mapboxService.map.on('load', () => {
+      this.mapboxService.addMarkers(this.userPois);
     });
-
-    // Add navigation control (zoom and rotation controls)
-    this.map.addControl(new mapboxgl.NavigationControl());
-
-    // Add full screen control
-    this.map.addControl(new mapboxgl.FullscreenControl());
-
-    // Add custom 3D toggle control
-    const toggle3DControl = this.create3DToggleControl();
-    this.map.addControl(toggle3DControl);
-
-    this.map.on('load', () => {
-      this.addMarkersToMap();
-    });
-  }
-
-  private addMarkersToMap(): void {
-    this.userPois.forEach((poi) => {
-      const color = poi.active ? '#5ee560' : '#e55e5e';
-      const el = document.createElement('div');
-      el.className = 'marker';
-      el.style.backgroundColor = color;
-      el.style.width = '15px';
-      el.style.height = '15px';
-      el.style.borderRadius = '50%';
-
-      const marker = new mapboxgl.Marker(el)
-        .setLngLat([poi.longitude, poi.latitude])
-        .setPopup(
-          new mapboxgl.Popup().setHTML(
-            `<b>${poi.titel}</b><br>${poi.description}`
-          )
-        )
-        .addTo(this.map);
-
-      this.markers[poi.id] = marker;
-    });
-  }
-
-  private create3DToggleControl() {
-    const controlDiv = document.createElement('div');
-    controlDiv.className = 'mapboxgl-ctrl';
-
-    const button = document.createElement('button');
-    button.textContent = 'Toggle 3D';
-    button.onclick = () => this.toggle3DMode();
-    controlDiv.appendChild(button);
-
-    return {
-      onAdd: () => controlDiv,
-      onRemove: () => controlDiv.parentNode?.removeChild(controlDiv),
-    };
   }
 
   private toggle3DMode(): void {
-    const layerId = '3d-buildings';
-
-    if (this.map.getLayer(layerId)) {
-      // Schalte die Sichtbarkeit der 3D-Gebäude-Schicht um
-      const visibility = this.map.getLayoutProperty(layerId, 'visibility');
-      this.map.setLayoutProperty(
-        layerId,
-        'visibility',
-        visibility === 'visible' ? 'none' : 'visible'
-      );
-      console.log('3D visibility toggled:', visibility);
-    } else {
-      // Füge die Quelle nur hinzu, wenn sie nicht bereits vorhanden ist
-      if (!this.map.getSource('composite')) {
-        this.map.addSource('composite', {
-          type: 'vector',
-          url: 'mapbox://mapbox.mapbox-streets-v8',
-        });
-      }
-      // Füge die 3D-Gebäude-Schicht hinzu
-      this.map.addLayer({
-        id: layerId,
-        source: 'composite',
-        'source-layer': 'building',
-        filter: ['==', 'extrude', 'true'],
-        type: 'fill-extrusion',
-        minzoom: 15,
-        paint: {
-          'fill-extrusion-color': '#aaa',
-          'fill-extrusion-height': [
-            'interpolate',
-            ['linear'],
-            ['zoom'],
-            15,
-            0,
-            15.05,
-            ['get', 'height'],
-          ],
-          'fill-extrusion-base': [
-            'interpolate',
-            ['linear'],
-            ['zoom'],
-            15,
-            0,
-            15.05,
-            ['get', 'min_height'],
-          ],
-          'fill-extrusion-opacity': 0.6,
-        },
-        layout: {
-          visibility: 'visible',
-        },
-      });
-      console.log('3D buildings layer added');
-    }
+    this.mapboxService.toggle3DMode();
   }
 
   private toggleAccordion(poiId: string): void {
@@ -235,7 +122,7 @@ export class ViewUserComponent {
       new Date().toISOString(),
       voteType,
       null, // Adjust here if necessary
-      null, // Adjust here if necessary
+      null, // Adjust hier if necessary
       poiId,
       this.user
     );
