3974ca9255366988f70642f8dcc8ff57829bddd1
MohamadAlaskari <m_alskari96@hotmail.com>
2024-05-25 14:28:51 +0200

    finish report service implementierung

--- src/app/modules/report-management/services/report-management.service.ts
+++ src/app/modules/report-management/services/report-management.service.ts
@@ -1,9 +1,47 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../../../../environment';
+import { ApiService } from '../../../core/Services/api.service';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ReportManagementService {
+  reportEndePoints = environment.endpoints.report;
 
-  constructor() { }
+  constructor(private apiService: ApiService) {}
+
+  getReports(): Observable<Report[]> {
+    return this.apiService.get<Report[]>(`${this.reportEndePoints.findAll}`);
+  }
+  getReportByReportId(reportId: string): Observable<Report> {
+    return this.apiService.get<Report>(
+      `${this.reportEndePoints.findById}/${reportId}`
+    );
+  }
+
+  getReportsByPoiId(poiId: string): Observable<Report[]> {
+    return this.apiService.get<Report[]>(
+      `${this.reportEndePoints.findRebortsByRebortedPoiId}/${poiId}`
+    );
+  }
+
+  getReportsByCommentId(commentId: string): Observable<Report[]> {
+    return this.apiService.get<Report[]>(
+      `${this.reportEndePoints.findRebortsByRebortedCommentId}/${commentId}`
+    );
+  }
+
+  getReportsByUserId(userId: string): Observable<Report[]> {
+    return this.apiService.get<Report[]>(
+      `${this.reportEndePoints.findRebortsByRebortedUserId}/${userId}`
+    );
+  }
+
+  updateReport(report: Report): Observable<Report> {
+    return this.apiService.put<Report>(
+      `${this.reportEndePoints.update}`,
+      report
+    );
+  }
 }
